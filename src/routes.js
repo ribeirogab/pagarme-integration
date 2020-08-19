29cfda5fce9d0ac131b9410962fc51e61960f9c2
ribeirogab <gblosr@gmail.com>
2020-08-19 19:50:01 -0300

    feat: add trycatch

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,19 @@ const { Router } = require('express');
 const paymentsRouter = Router();
 
 paymentsRouter.post('/payments', async (req, res) => {
-  const client = await pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY });
+  try {
+    const client = await pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY });
 
-  const transactions = await client.capture({ id: '1627822' });
-
-  return res.json(transactions);
+    const transactions = await client.capture({ id: '1627822' });
+  
+    return res.json(transactions);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 paymentsRouter.get('/payments', (req, res) => {
-  return res.json({ 
-    PAGARME_API_KEY: process.env.PAGARME_API_KEY,
-    PAGARME_ENCRYPTION_KEY: process.env.PAGARME_ENCRYPTION_KEY,
-  });
+  return res.json({});
 });
 
 module.exports = paymentsRouter;
