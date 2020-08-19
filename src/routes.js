dfa0fbc840f47f66692bf082ff2e5fdf14ed12e0
ribeirogab <gblosr@gmail.com>
2020-08-19 19:46:59 -0300

    feat: add keys of pagarme

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,19 @@ const { Router } = require('express');
 
 const paymentsRouter = Router();
 
-paymentsRouter.post('/payments', (req, res) => {
+paymentsRouter.post('/payments', async (req, res) => {
+  const client = await pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY });
 
+  const transactions = await client.capture({ id: '1627822' });
+
+  return res.json(transactions);
 });
 
 paymentsRouter.get('/payments', (req, res) => {
-  return res.json({ ok: true });
+  return res.json({ 
+    PAGARME_API_KEY: process.env.PAGARME_API_KEY,
+    PAGARME_ENCRYPTION_KEY: process.env.PAGARME_ENCRYPTION_KEY,
+  });
 });
 
 module.exports = paymentsRouter;
