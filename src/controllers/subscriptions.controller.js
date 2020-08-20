70bc3b2a5a07b3c8ee7202e8788f8ed65cc5c173
ribeirogab <gblosr@gmail.com>
2020-08-20 14:03:39 -0300

    fix: add Procfile and remove duplicate vars

--- src/controllers/subscriptions.controller.js
+++ src/controllers/subscriptions.controller.js
@@ -21,10 +21,6 @@ class PaymentsController extends Controller {
 
       if (!selectedPlan) throw Error('Plan does not exist!');
 
-      const client = await pagarme.client.connect({
-        api_key: process.env.PAGARME_API_KEY
-      });
-
       const transaction = await client.transactions.capture({
         id: token, amount: selectedPlan.amount,
       });
@@ -34,7 +30,7 @@ class PaymentsController extends Controller {
       let paymentWithCreditCard = {};
 
       if (transaction.payment_method === 'credit_card') {
-        const client_encrypt = pagarme.client.connect({ encryption_key: process.env.PAGARME_API_KEY });
+        const client_encrypt = pagarme.client.connect({ encryption_key: process.env.PAGARME_ENCRYPTION_KEY });
 
         const card_hash = client_encrypt.security.encrypt({
           // card_number: transaction.card.holder_name,
@@ -42,7 +38,6 @@ class PaymentsController extends Controller {
           card_expiration_date: transaction.card.expiration_date,
           // card_cvv: transaction.card.holder_name,
         });
-
       }
 
       const subscription = await client.subscriptions.create({
